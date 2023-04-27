762e20522e88d21c35629c87195ad63bd9ffaa2f
Rodion <pablotapias98@gmail.com>
2023-04-27 18:35:06 -0500

    feat: Agregar función para obtener la dirección IP pública
    Se agregó una función para obtener la dirección IP pública de la máquina utilizando el servicio 'checkip.amazonaws.com'. Se manejan errores de timeout y de solicitud utilizando la librería 'requests' y se añadió un archivo de registro 'logger' para registrar dichos errores.

--- ip-publica.py
+++ ip-publica.py
@@ -1,3 +1,19 @@
 import requests
-ip = requests.get('http://checkip.amazonaws.com').text.strip()
-print(ip)
+import logger
+
+def get_my_ip(): # This function returns the public IP address of the machine.
+    CHECK_IP_URL = 'http://checkip.amazonaws.com'
+    try:
+        TIMEOUT = 5
+        ip = requests.get(CHECK_IP_URL, timeout=TIMEOUT).text.strip()
+        return ip
+    except requests.exceptions.Timeout as e:
+        logger.error("Timeout error: %s", e)
+        ip = None
+        return ip
+    except requests.exceptions.RequestException as e:
+        logger.error("Request error: %s", e)
+        ip = None
+        return ip
+
+get_my_ip()
